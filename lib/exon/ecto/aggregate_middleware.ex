ea12c6b6035023d6f87bb4aebd7eaf8d53e61735
Simon Vaucher <s.vaucher@applicaster.com>
2017-05-10 15:50:14 +0300

    add put-delete action

--- lib/exon/ecto/aggregate_middleware.ex
+++ lib/exon/ecto/aggregate_middleware.ex
@@ -2,7 +2,7 @@ defmodule Exon.Ecto.AggregateMiddleware do
   use Exon.Middleware
   @private_key :ecto_aggregate
 
-  defmodule MissingAggregareIdError do
+  defmodule MissingAggregateIdError do
     defexception message: """
       Command is missing :aggregate_id
       There are two way to specify :aggregate_id
@@ -18,6 +18,15 @@ defmodule Exon.Ecto.AggregateMiddleware do
 
   def get_changeset(%Command{private: private}) do
     get_in(private, [Access.key(@private_key, %{}), :changeset])
+  end
+
+  def get_action(%Command{private: private}) do
+    get_in(private, [Access.key(@private_key, %{}), :action])
+  end
+
+  def put_delete(%Command{private: private} = command, _aggregate) do
+    private = put_in(private, [Access.key(@private_key, %{}), :action], :delete)
+    %{command | private: private}
   end
 
   def init(opts) do
@@ -37,16 +46,31 @@ defmodule Exon.Ecto.AggregateMiddleware do
     if ecto_aggregate?(module) do
       case get_changeset(command) do
         %Ecto.Changeset{} = changeset ->
-          save_and_alter_result(command, changeset, repo)
-        _ -> command
+          perform_insert_or_update(command, changeset, repo)
+        _ ->
+          case get_action(command) do
+            :delete ->
+              perform_delete(command, repo)
+            _ -> command
+          end
       end
     else
       command
     end
   end
 
-  defp save_and_alter_result(%{result: result} = command, changeset, repo) do
-    case repo.insert_or_update(changeset) do
+  defp perform_insert_or_update(command, changeset, repo) do
+    command
+    |> handle_repo_result(repo.insert_or_update(changeset))
+  end
+
+  defp perform_delete(command, repo) do
+    command
+    |> handle_repo_result(repo.delete(command.aggregate))
+  end
+
+  defp handle_repo_result(%{result: result} = command, repo_response) do
+    case repo_response do
       {:ok, aggregate} ->
         if result == nil do
           %{command | result: {:ok, aggregate}}
@@ -66,7 +90,7 @@ defmodule Exon.Ecto.AggregateMiddleware do
     if spec[:new] do
       struct(aggregate_module)
     else
-      unless spec[:aggregate_id], do: raise(MissingAggregareIdError)
+      unless spec[:aggregate_id], do: raise(MissingAggregateIdError)
       id = payload[spec[:aggregate_id]]
       query = aggregate_module.get(id)
       if spec[:allow_nil], do: repo.one(query), else: repo.one!(query)
